a543aa218643fb06fc8eac65199d4fcd2eb237a6
Mike Calabrese <mikeclbrs@gmail.com>
2018-05-12 17:46:27 -0400

    added in service and quote box

--- header.php
+++ header.php
@@ -6,16 +6,18 @@
 		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() ?>/static/stylesheets/css/bootstrap.css">
 
 		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() ?>/style.css">
+
+
 	</head>
 	
 	<body>
 
 	<div class = "container">
 
-	<header class = " row site-header">
+	<header class = "site-header">
 			<img class="col-4" src="<?php echo get_template_directory_uri() ?>/static/images/WillowBrookLabsLogo.png">
 
-			<nav class="site-nav col-4 offset-6">
+			<nav class="nav-menu">
 				<?php
 
 				$args = array(
--- index.php
+++ index.php
@@ -11,6 +11,26 @@
 	<img class="jumbo center-block" src="<?php echo get_template_directory_uri() ?>/static/images/motherboardJumbo.jpg">
 </div>
 
+
+<div class="row info-boxes">
+	
+	<div class=" col-4 offset-2 contact-box text-center"> 
+		<h1 class="contact-box-header">Service</h1>
+
+		<p class="text-center"> Willowbrook Labs offer a variety of digital solutions that help you make your organization more productive. Let us help build your technology infrastructure so you can get back to the important business of running your organization.  </p>
+	</div>
+
+	<div class=" col-4 offset-1 quote-box"> 
+		<h1 class=" quote-box-header text-center"> Get A Free Quote</h1>
+
+		<p class="text-center"> Feel free to reach out and get a quote </p>
+	</div>
+
+
+</div>
+
+
+
 <?php get_footer();?>
 
 
