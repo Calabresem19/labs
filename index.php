919ca04434086b68abf1f6544213c264fd9cfd50
Mike Calabrese <mikeclbrs@gmail.com>
2018-05-21 00:18:33 -0400

    progress on homepage

--- index.php
+++ index.php
@@ -6,19 +6,25 @@
 
 
 <div class = "home-page-image row">
-	<img class="col-12" src="<?php echo get_template_directory_uri() ?>/static/images/motherboardJumbo.jpg">
+	<img class="col-12" src="<?php echo get_template_directory_uri() ?>/static/images/WillowBrookLabsMotherboard.png">
 </div>
 
 
+<div class="row statment">
+	<div class="center-block">
+		<h3 class="text-center "> Willowbrook Labs Provides State Of The Arc Technology Solutions</h3>
+	</div>
+</div>
+
 <div class="row info-boxes">
 	
-	<div class=" col-4 offset-2 contact-box text-center"> 
+	<div class=" col-4 offset-1 contact-box text-center"> 
 		<h1 class="contact-box-header">Services</h1>
 
-		<p class="text-center"> Willowbrook Labs offer a variety of digital solutions that help you make your organization more productive. Let us help build your technology infrastructure so you can get back to the important business of running your organization.  </p>
+		<p class="text-center "> Willowbrook Labs offer a variety of digital solutions that help you make your organization more productive. Let us help build your technology infrastructure so you can get back to the important business of running your organization.  </p>
 	</div>
 
-	<div class=" col-4 offset-1 quote-box"> 
+	<div class=" col-4 offset-2 quote-box"> 
 		<h1 class=" quote-box-header text-center"> Get A Free Quote</h1>
 
 		<p class="text-center"> Feel free to reach out and get a quote </p>
