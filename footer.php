cde0746625be84f68b13589bb78d3080f7053f9f
Mike Calabrese <mikeclbrs@gmail.com>
2018-05-26 16:40:19 -0400

    issues with footer

--- footer.php
+++ footer.php
@@ -1,27 +1,39 @@
-	<footer class = "site-footer row">
+	<footer class = "site-footer ">
+
+<!--
+	<div class="row">
+	<p class="col-1">test</p>
+	<p class="col-1">test</p>
+	<p class="col-1">test</p>
+	<p class="col-1">test</p>
+	<p class="col-1">test</p>
 
 
+	</div>
 
-
-		<div class="social-media col-5 row "> 
-			<p class="Follow-Us col-1"> FOLLOW US | </p>
-			<ul class="social-media col-2">
-				<li> <a href="#">TWITTER</a> </li>
-				<li> <a href="#">INSTAGRAM</a> </li>
-			</ul>
+-->
+		<div class="social-media row ">
+<!--				<div class="col-11"> -->
+					<p class="Follow col-1"> FOLLOW US |</p> 
+					<ul class="social-media-list col-2  ">
+						<li> <a href="#">TWITTER</a> </li>
+						<li> <a href="#">INSTAGRAM</a> </li>
+					</ul>
+<!--				</div> -->
 		</div>
+<!--
 			<nav class=" col-2 offset-7 nav-menu-footer">
-				<?php
+				<?php /*
 
 				$args = array(
 					'theme_location' => 'primary'
 				);
 				
-				?>
+				*/?>
 				<?php wp_nav_menu($args) ?>
 			</nav>
 
-
+-->
 	</footer>
 	</body>
 </html>
