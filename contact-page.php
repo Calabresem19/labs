7366174d19f0f701788343a4e02d46017999584e
Mike Calabrese <mikeclbrs@gmail.com>
2018-06-13 02:27:35 -0400

    better styled contact page

--- contact-page.php
+++ contact-page.php
@@ -11,25 +11,35 @@
 
 
 <div id="contact-form-row"  class="row">
-	<div id="contact-form" class= "offset-3 col-6 "> 
+	<div id="contact-form" class= "offset-2 col-8 "> 
 		
 		<div id="message-row" class="row">
-			<h3 class="offset-7">Message</h3>		
+			<h3 class="center-block">Message</h3>		
 		</div>
 		<div class="row">
 
-			<form>
-			Name:<br>
-			<input type="text" name="Name"><br>
-			Phone Number:<br>
-			<input type="text" name="PhoneNumber"><br>
-			Email:<br>
-			<input type="text" name="Email"><br>
-			<input type="text" name="Message"><br>
-
-
-			
-
+			<form class="col-12">
+				<div class="row">
+					<div class="col-5">
+						<div id="name-div" class="form-divs">
+							<label class="col-3 ">Name:</label>
+							<input class="col-6 "type="text" name="Name"><br>
+						</div>
+						<div id="phone-div" class="form-divs">
+							<label class="col-3 ">Phone:</label>
+							<input class="col-6" type="text" name="PhoneNumber"><br>
+						</div>
+						<div id="email-div" class="form-divs">
+							<label class="col-3 ">Email:</label>
+							<input class="col-6 " type="text" name="Email"><br>
+						</div>
+					</div>	
+					<div id="message-input" class="col-7 ">
+						<div class="row">
+							<textarea rows="7" class="col-11" name="Message" ></textarea>
+						</div>
+					</div>
+				</div>
 			</form>
 		</div>
 
