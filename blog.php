c12a913dc4f1b38369cde2dfe2f6c74d4cbd5583
Mike Calabrese <mikeclbrs@gmail.com>
2018-06-30 19:41:26 -0400

    fixed some spaceing with post

--- blog.php
+++ blog.php
@@ -5,26 +5,99 @@
 <?php get_header() ?>
 
 
-<div class="row">
-	<div id="posts" class="center-block">
-		<?php
+
+
+
+<div class="row row-for-top-three ">
+	<div class="top-three-posts offset-1  col-3 ">
+		<div class="row">
+			<?php
 		    $args = array(
-			'post_type' => 'post'
+			'post_type' => 'post',
+			'orderby' => 'data'
 		    );
 
 		    $post_query = new WP_Query($args);
 			if($post_query->have_posts() ) {
 			  while($post_query->have_posts() ) {
 			    $post_query->the_post();
+
+				if ($post_query->current_post == 0){ ?>
+					<h2 class="headingTopThree"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+		
+				<?php }
+
 			    ?>
-				<h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 
 			    <?php
 				  }
 			}
 		?>
+		</div>
 	</div>
+	<div class="top-three-posts col-3 ">
+		<div class="row">
+			<?php
+					    $args = array(
+						'post_type' => 'post',
+						'orderby' => 'data'
+					    );
+
+					    $post_query = new WP_Query($args);
+						if($post_query->have_posts() ) {
+						  while($post_query->have_posts() ) {
+						    $post_query->the_post();
+
+							if ($post_query->current_post == 1){ ?>
+								<h2 class="headingTopThree"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+					
+							<?php }
+
+						    ?>
+
+						    <?php
+							  }
+						}
+					?>
+
+
+		</div>
+	</div>
+	<div class="top-three-posts col-3 ">
+
+		<div class="row">
+		<?php
+				    $args = array(
+						'post_type' => 'post',
+						'orderby' => 'data'
+					    );
+
+					    $post_query = new WP_Query($args);
+						if($post_query->have_posts() ) {
+						  while($post_query->have_posts() ) {
+						    $post_query->the_post();
+
+							if ($post_query->current_post == 2){ ?>
+								<h2 class="headingTopThree"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+					
+							<?php }
+
+						    ?>
+
+						    <?php
+							  }
+						}
+		?>
+		</div>
+	</div>
+
 </div>
+
+
+
+
+
+
 
 
 
--- functions.php
+++ functions.php
@@ -11,4 +11,43 @@ add_action( 'init', 'register_my_menu' );
 add_theme_support( 'post-thumbnails' ); 
 
 
+
+
+function test_func() {
+	
+	echo "this is a test"; 
+
+}
+
+
+
+function grapPostFunc() {
+					    $args = array(
+						'post_type' => 'post',
+						'orderby' => 'data'
+					    );
+
+					    $post_query = new WP_Query($args);
+						if($post_query->have_posts() ) {
+						  while($post_query->have_posts() ) {
+						    $post_query->the_post();
+
+							if ($post_query->current_post == 2){ ?>
+								<h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+					
+							<?php }
+
+						    ?>
+
+						    <?php
+							  }
+						}
+
+
+}
+	
+
+
+
+
 ?>
