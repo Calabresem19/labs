b5fd4c675faa4fb01030d4194c822d3474a9e9d1
Mike Calabrese <mikeclbrs@gmail.com>
2018-07-31 23:58:40 -0400

    put in links to inde.php boxes

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 	<div class=" col-4 offset-1 main-boxes contact-box row"> 
 				<h1 class="contact-box-header col-12 text-center">Services</h1>
 			<p class="text-center "> Willowbrook Labs offer a variety of digital solutions that help you make your organization more productive. Let us help build your technology infrastructure so you can get back to the important business of running your organization.  </p>
-			<h1 id="about-us-link" class="text-center col-12 text-center"><a href="#"> About Us<span style="color:#e29860;">&#10142</span></a> </h1> 
+			<h1 id="about-us-link" class="text-center col-12 text-center"><a href="<?php echo(home_url())?>/index.php/about/"> About Us<span style="color:#e29860;">&#10142</span></a> </h1> 
 		
 	</div>
 
@@ -26,7 +26,7 @@
 			<h1 class=" quote-box-header col-12 text-center"> Get A Free Quote</h1>
 
 		<p class="text-center col-12 text-center"> Feel free to reach out and get a quote </p>
-		<h1 id="contact-us-link" class="text-center col-12 text-center"><a href="#"> Contact Us<span style="color:#e29860;">&#10142</span></a> </h1> 
+		<h1 id="contact-us-link" class="text-center col-12 text-center"><a href="<?php echo(home_url())?>/index.php/contact/"> Contact Us<span style="color:#e29860;">&#10142</span></a> </h1> 
 
 	</div>
 </div>
