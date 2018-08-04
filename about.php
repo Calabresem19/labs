3c1e96c3e2302990a1d14761f109038563b51920
Mike Calabrese <mikeclbrs@gmail.com>
2018-08-04 19:47:17 -0400

    working on about page

--- about.php
+++ about.php
@@ -2,14 +2,30 @@
 
 
 <?php get_header() ?>
-
+<div id="about-wrapper" class= "center-block">
 <div class="row">
-	<div class="">
-
-		<?php while(have_posts()) : the_post(); ?>
-		<?php the_content();?>
-		<?php endwhile; ?>
+	<div class="about-title offset-3 col-6 text-center">
+		<h1>Building Your Products of the Future</h1>
 	</div>
 </div>
 
+
+
+
+<div id="about-info" class="row ">
+	<div class="offset-1 col-10 row ">
+		<?php while(have_posts()) : the_post(); ?>
+		<div id="about-paragraph"><?php the_content();?></div>
+	</div>
+		<div class="row offset-1 col-10 " id="about-image-row">
+			<img id="business-image" class="img-fluid col-5" src="<?php echo get_template_directory_uri() ?>/static/images/class-start.jpg">
+			<h1 id="work-with-you"  class=" offset-1 col-6 text-center "> WE WANT TO WORK WITH YOU </h1>
+		</div>
+		<?php endwhile; ?>
+
+		<div class="row offset-1 col-10" >
+			<h1 id="contact-us-link" class="text-center col-12 text-center"><a href="<?php echo(home_url())?>/index.php/contact/"> Contact Us<span style="color:#e29860;">&#10142</span></a> </h1> 	
+		</div>
+
+</div>
 <?php get_footer() ?>
